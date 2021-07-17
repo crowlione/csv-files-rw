2730c7a42e83cf0749f3a69baf19fffaced30592
crowlione <katy.gerasimova99@gmail.com>
2021-07-17 18:28:25 +0300

    added mail sending with gmail smtp

--- Hitachi_Solutions_Assessment/Program.cs
+++ Hitachi_Solutions_Assessment/Program.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 
 namespace Hitachi_Solutions_Assessment
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             String fileName;
@@ -36,19 +38,33 @@ namespace Hitachi_Solutions_Assessment
                 Console.WriteLine(p.country);
             }
 
-            //using (var writer = new StreamWriter(@"C:\Users\Katy\Desktop\test.txt"))
-            //using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-            //{
-            //    csv.WriteRecords(allRecords);
-            //}
+            using (var writer = new StreamWriter(@"C:\Users\Katy\Desktop\test.txt"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(allRecords);
+            }
+            Program program = new Program();
+            program.email_send();
+        }
 
+        public void email_send()
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            mail.From = new MailAddress("eg.phpmails@gmail.com");
+            mail.To.Add("kyashicrow@gmail.com");
+            mail.Subject = "Test Mail - 1";
+            mail.Body = "mail with attachment";
 
+            System.Net.Mail.Attachment attachment;
+            attachment = new System.Net.Mail.Attachment(@"C:\Users\Katy\Desktop\test.txt");
+            mail.Attachments.Add(attachment);
 
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("eg.phpmails@gmail.com", "killmyself!1");
+            SmtpServer.EnableSsl = true;
 
-
-
-
-
+            SmtpServer.Send(mail);
 
         }
     }
