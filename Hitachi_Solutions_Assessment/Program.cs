6a2e60b8fdab2eed96ff879867c1cc1d15434fef
crowlione <katy.gerasimova99@gmail.com>
2021-07-18 14:35:11 +0300

    Added input fields, DE/EN language and exceptions

--- Hitachi_Solutions_Assessment/Program.cs
+++ Hitachi_Solutions_Assessment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.VisualBasic.FileIO;
 using System;
 using System.Collections.Generic;
@@ -15,86 +16,139 @@ namespace Hitachi_Solutions_Assessment
        
         static void Main(string[] args)
         {
+            
             String fileName;
             String senderEmailAddress;
             String password;
             String receiverEmailAddress;
-            String[] textBoxes = new String[] { "Cupcake", "Cake", "Candy" }; 
-            
-            //TO BE variable fileName
-            var path = @"C:\Users\Katy\Desktop\testfile.csv";
 
-            List<Person> allRecords;
-
-            using (var reader = new StreamReader(path))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            do
             {
-                var records = csv.GetRecords<Person>();
-                allRecords = records.ToList();
+                Console.WriteLine("Please select a language. Type in either EN or DE.\n" +
+                   "Bitte wählen Sie eine Sprache. Geben Sie entweder EN oder DE ein.\n");
+                Globals.LANGUAGE = Console.ReadLine().ToUpper();
+            }
+            while (Globals.LANGUAGE != "DE" && Globals.LANGUAGE != "EN");
+
+            if (Globals.LANGUAGE == "DE")
+            {
+                Globals.changeLanguage();
             }
 
-            // this is IEnumerable
-            var newList = allRecords.GroupBy(r => r.country)
-                     .Select(s => new { Country = s.Key, 
-                         Avg = s.Average(p => p.score), 
-                         Max = s.Max(p => p.score), 
-                         Min = s.Min(p => p.score),
-                         Count = s.Count()})
-                     .OrderBy(s=>s.Avg);
+            Console.WriteLine(Globals.FILE_NAME);
+            
+            fileName = Console.ReadLine();
 
-            List<Record> results = new List<Record>();
+            List<Person> allRecords = new List<Person>();
 
-            Person maxPerson;
-            Person minPerson;
-            double median;
 
-            foreach (var p in newList)
+            var config = new CsvConfiguration(CultureInfo.CurrentCulture) { Delimiter = ";"};
+            try
             {
-                maxPerson = allRecords.Find(r => r.score == p.Max);
-                minPerson = allRecords.Find(r => r.score == p.Min);
-                median = allRecords.FindAll(r => r.country == p.Country)
-                    .OrderByDescending(r => r.score)
-                    .ElementAt(p.Count % 2 == 0 ? p.Count/2 : (p.Count-1)/2).score;
+                using (var reader = new StreamReader(fileName)) 
+                       
+                using (var csv = new CsvReader(reader, config))
+                {
+                    var records = csv.GetRecords<Person>();
+                    allRecords = records.ToList();
+                }
+           
+
+                // this is IEnumerable
+                if(allRecords.Count() == 0)
+                {
+                    Console.WriteLine(Globals.EMPTY_WARNING);
+                }
+
+                if (allRecords.Count() != 0)
+                {
+                    var newList = allRecords.GroupBy(r => r.country)
+                         .Select(s => new { Country = s.Key, 
+                             Avg = s.Average(p => p.score), 
+                             Max = s.Max(p => p.score), 
+                             Min = s.Min(p => p.score),
+                             Count = s.Count()})
+                         .OrderByDescending(s=>s.Avg);
+
+                List<Record> results = new List<Record>();
+
+                Person maxPerson;
+                Person minPerson;
+                double median;
+
+                foreach (var p in newList)
+                {
+                    maxPerson = allRecords.Find(r => r.score == p.Max);
+                    minPerson = allRecords.Find(r => r.score == p.Min);
+                    median = allRecords.FindAll(r => r.country == p.Country)
+                        .OrderByDescending(r => r.score)
+                        .ElementAt(p.Count % 2 == 0 ? p.Count/2 : (p.Count-1)/2).score;
        
 
-                results.Add(new Record(p.Country, p.Avg, median, p.Max, maxPerson.firstName + " " + maxPerson.lastName, 
-                    p.Min, minPerson.firstName + " " + minPerson.lastName, p.Count));
-            }
+                    results.Add(new Record(p.Country, p.Avg, median, p.Max, maxPerson.firstName + " " + maxPerson.lastName, 
+                        p.Min, minPerson.firstName + " " + minPerson.lastName, p.Count));
+                }
 
-            foreach (Record r in results)
+                using (var writer = new StreamWriter("ReportByCountry.csv"))
+                    using (var csv = new CsvWriter(writer, config))
+                {
+                    csv.WriteRecords(results);
+                }
+
+                    Console.WriteLine(Globals.EMAIL);
+                    senderEmailAddress = Console.ReadLine();
+
+                    Console.WriteLine(Globals.PASSWORD);
+                    password = Console.ReadLine();
+
+                    Console.WriteLine(Globals.RECEIVER_EMAIL);
+                    receiverEmailAddress = Console.ReadLine();
+
+                    Program program = new Program();
+                    try
+                    {
+                        program.email_send(senderEmailAddress, password, receiverEmailAddress);
+                    }
+                     catch (FormatException)
+                    {
+                        Console.WriteLine(Globals.FORMAT_ERROR);
+                    }
+                }
+            }
+            catch (FileNotFoundException)
             {
-                Console.WriteLine(r.averageScore + " " + r.country + " " + r.recordCount + " " + r.maxScore + " " + r.maxScorePerson + " "
-                    + r.minScore + " " + r.minScorePerson + " " + r.medianScore);
+                Console.WriteLine(Globals.FILE_NOT_FOUND);
             }
-
-            //using (var writer = new StreamWriter("ReportByCountry.csv"))
-            //using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-            //{
-            //    csv.WriteRecords(allRecords);
-            //}
-            //Program program = new Program();
-            // program.email_send();
         }
 
-        //public void email_send()
-        //{
-        //    MailMessage mail = new MailMessage();
-        //    SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-        //    mail.From = new MailAddress("eg.phpmails@gmail.com");
-        //    mail.To.Add("kyashicrow@gmail.com");
-        //    mail.Subject = "Test Mail - 1";
-        //    mail.Body = "mail with attachment";
+        public void email_send(String senderEmail, String senderPassword, String receiverEmail)
+        {
+            
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress(senderEmail);
+                mail.To.Add(receiverEmail);
+                mail.Subject = Globals.EMAIL_TITLE;
+                mail.Body = Globals.GREETING + senderEmail + Globals.EMAIL_BODY;
 
-        //    System.Net.Mail.Attachment attachment;
-        //    attachment = new System.Net.Mail.Attachment(@"test.csv");
-        //    mail.Attachments.Add(attachment);
+                System.Net.Mail.Attachment attachment;
+                attachment = new System.Net.Mail.Attachment("ReportByCountry.csv");
+                mail.Attachments.Add(attachment);
 
-        //    SmtpServer.Port = 587;
-        //    SmtpServer.Credentials = new System.Net.NetworkCredential(email, password);
-        //    SmtpServer.EnableSsl = true;
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(senderEmail, senderPassword);
+                SmtpServer.EnableSsl = true;
 
-        //    SmtpServer.Send(mail);
+               try
+               {
+                   SmtpServer.Send(mail);
+               }
+               catch (SmtpException)
+               {
+                   Console.WriteLine(Globals.UNAUTHORIZED_ERROR);
+               }
+         
 
-        //}
+        }
     }
 }
